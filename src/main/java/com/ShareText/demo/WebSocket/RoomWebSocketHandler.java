6e9f6fcb69ae5b7f46d3fe7ff34ca32704dc4f33
Suryansh Bakliwal <sbakliwal9@gmail.com>
2026-04-13 22:29:27 +0530

    redis enhancement to reduce db calls

--- src/main/java/com/ShareText/demo/WebSocket/RoomWebSocketHandler.java
+++ src/main/java/com/ShareText/demo/WebSocket/RoomWebSocketHandler.java
@@ -7,45 +7,89 @@ import com.ShareText.demo.services.RoomService.RoomServiceImpl;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 import org.springframework.web.socket.TextMessage;
+
+import java.time.Duration;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.springframework.data.redis.core.StringRedisTemplate;
+
 @Component
 public class RoomWebSocketHandler extends TextWebSocketHandler {
-    private final Map<String, List<WebSocketSession>> rooms = new HashMap<>();
+    private final Map<String, List<WebSocketSession>> rooms = new ConcurrentHashMap<>();
+
+
+    private final StringRedisTemplate redisTemplate;
+    private final Map<String, Timer> roomTimers = new ConcurrentHashMap<>();
     private final Map<String, String> roomBuffer = new ConcurrentHashMap<>();
     private final Set<String> dirtyRooms = ConcurrentHashMap.newKeySet();
 
     private final RoomService roomService;
 
-    public RoomWebSocketHandler(RoomService roomService) {
+    public RoomWebSocketHandler(StringRedisTemplate redisTemplate, RoomService roomService) {
+        this.redisTemplate = redisTemplate;
         this.roomService = roomService;
     }
 
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) {
+        String roomCode = getRoomCode(session);
+
+        rooms.computeIfAbsent(roomCode,
+                        k -> Collections.synchronizedList(new ArrayList<>()))
+                .add(session);
+
+        System.out.println("User joined room: " + roomCode);
+    }
 
     @Override
-    public void afterConnectionEstablished(WebSocketSession session){
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
         String roomCode = getRoomCode(session);
-        rooms.computeIfAbsent(roomCode, k -> new ArrayList<>()).add(session);
-        System.out.println("User joined room: " + roomCode);
+
+        saveToDB(roomCode);
+        List<WebSocketSession> sessions = rooms.get(roomCode);
+        if (sessions != null) {
+            sessions.remove(session);
+            if (sessions.isEmpty()) {
+                rooms.remove(roomCode);
+            }
+        }
     }
 
 
     @Override
-    public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception{
+    public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         String roomCode = getRoomCode(session);
         String content = message.getPayload();
 
-        roomBuffer.put(roomCode, content);
-        dirtyRooms.add(roomCode);
+        //store in redis
+        redisTemplate.opsForValue().set("room:" + roomCode, content, Duration.ofMinutes(10));
+
+        //debounce DB save
+        if(roomTimers.containsKey(roomCode)){
+            roomTimers.get(roomCode).cancel();
+        }
+        Timer timer = new Timer();
+
+        timer.schedule(new TimerTask(){
+            @Override
+            public void run(){
+                saveToDB(roomCode);
+            }
+        }, 3000);
+
+        // 🔥 3. Broadcast to others
         for (WebSocketSession s : rooms.getOrDefault(roomCode, new ArrayList<>())) {
-            if(s.isOpen() && !s.getId().equals(session.getId())){
+            if (s.isOpen() && !s.getId().equals(session.getId())) {
                 s.sendMessage(message);
+                System.out.println("Hello message getting broadcast");
             }
         }
+
     }
 
     private String getRoomCode(WebSocketSession session) {
@@ -53,14 +97,26 @@ public class RoomWebSocketHandler extends TextWebSocketHandler {
     }
 
 
-    @Scheduled(fixedRate = 5000)
-    public void updateContentInRoom(){
-        for(String room : dirtyRooms){
-            String content = roomBuffer.get(room);
+    private void saveToDB(String roomCode) {
+        String content = redisTemplate.opsForValue().get("room:" + roomCode);
 
-            if(content == null) continue;
-            this.roomService.updateContentInRoom(room, content);
+        if (content != null) {
+            roomService.updateContentInRoom(roomCode, content);
+            System.out.println("Save to DB : " + roomCode);
         }
     }
+
+
+    // without redis
+//    @Scheduled(fixedRate = 5000)
+//    public void updateContentInRoom() {
+//        for (String room : dirtyRooms) {
+//            String content = roomBuffer.get(room);
+//
+//            if (content == null) continue;
+//            this.roomService.updateContentInRoom(room, content);
+//            dirtyRooms.remove(room);
+//        }
+//    }
 
 }
